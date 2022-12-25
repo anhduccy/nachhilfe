6a1572d3b6a7a8bb2ab34f3e7f5527c345cfb25b
Duc Nhat Anh Nguyen <nducnhatanh@icloud.com>
2022-12-25 16:05:34 +0100

    Add BarChart to SalesView

--- nachhilfe/Views/ContentView/SalesView.swift
+++ nachhilfe/Views/ContentView/SalesView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Charts
 import RealmSwift
 
 class MonthNavigator{
@@ -43,64 +44,79 @@ struct SalesView: View {
     @State var selectedMonth: Int = MonthNavigator.getCurrentMonth(date: Date())
     
     var body: some View {
-        ZStack{
+        GeometryReader{ geo in
             VStack{
                 ViewHeader("Umsatzhistorie", hasButton: false, action: {})
-                
-                VStack(spacing: 20){
-                    HStack(spacing: 10){
-                        if showAllSales{
-                            Text("Gesamt").font(.title2.bold())
-                        } else {
-                            HStack(spacing: 3){
-                                Button(action: {
-                                    selectedMonth -= 1
-                                }, label: {
-                                    Icon2(systemName: "chevron.backward", size: 27.5)
-                                })
-                                
-                                Button(action: {
-                                    selectedMonth += 1
-                                }, label: {
-                                    Icon2(systemName: "chevron.forward", size: 27.5)
-                                })
+                HStack{
+                    VStack(spacing: 20){
+                        HStack(spacing: 10){
+                            if showAllSales{
+                                Text("Gesamt").font(.title2.bold())
+                            } else {
+                                HStack(spacing: 3){
+                                    Button(action: {
+                                        selectedMonth -= 1
+                                    }, label: {
+                                        Icon2(systemName: "chevron.backward", size: 27.5)
+                                    })
+                                    
+                                    Button(action: {
+                                        selectedMonth += 1
+                                    }, label: {
+                                        Icon2(systemName: "chevron.forward", size: 27.5)
+                                    })
+                                }
+                                Text(MonthNavigator.getMonth(input: selectedMonth) + " " + MonthNavigator.getYear(input: selectedMonth)).font(.title2.bold())
                             }
-                            Text(MonthNavigator.getMonth(input: selectedMonth) + " " + MonthNavigator.getYear(input: selectedMonth)).font(.title2.bold())
-                        }
-                        
-                        Spacer()
-                        
-                        Button(showAllSales ? "Kalender anzeigen" : "Gesamtumsatz anzeigen"){
-                            showAllSales.toggle()
-                        }
-                    }
-                    
-                    VStack(spacing: 7.5){
-                        ForEach(students, id: \.self){ student in
-                            HStack{
-                                Text(student.surname + " " + student.name)
-                                Spacer()
-                                Text(salesPerStudent(student, selectedMonth: $selectedMonth).formatted(.currency(code: "EUR")))
-                            }.font(.body)
-                        }
-                        Divider()
-                        HStack{
-                            Text("Gesamt")
+                            
                             Spacer()
-                            Text(salesTotal().formatted(.currency(code: "EUR")))
-                        }.font(.body.weight(.bold))
-                    }
-                    Spacer()
-                }.padding()
+                            
+                            Button(showAllSales ? "Monate" : "Gesamt"){
+                                showAllSales.toggle()
+                            }.foregroundColor(.teal)
+                        }
+                        
+                        VStack(spacing: 7.5){
+                            ForEach(students, id: \.self){ student in
+                                HStack{
+                                    Text(student.surname + " " + student.name)
+                                    Spacer()
+                                    Text(salesPerStudent(student, selectedMonth: selectedMonth).formatted(.currency(code: "EUR")))
+                                }.font(.body)
+                            }
+                            Divider()
+                            HStack{
+                                Text("Gesamt")
+                                Spacer()
+                                Text(salesTotal(selectedMonth).formatted(.currency(code: "EUR")))
+                            }.font(.body.weight(.bold))
+                        }
+                        Spacer()
+                    }.padding()
+                        .frame(width: geo.size.width/2)
+                    ZStack{
+                        Chart{
+                            BarMark(x: .value("Monate", MonthNavigator.getMonth(input: selectedMonth) + " " + MonthNavigator.getYear(input: selectedMonth)) , y: .value("Umsatz", salesTotal(selectedMonth)))
+                                .foregroundStyle(by: .value("Ausgewählter Monat", "Ausgewählt"))
+                            ForEach(1...3, id: \.self){ data in
+                                BarMark(x: .value("Monate", MonthNavigator.getMonth(input: selectedMonth-data)  + " " + MonthNavigator.getYear(input: selectedMonth-data)) , y: .value("Umsatz", salesTotal(selectedMonth-data)))
+                                    .foregroundStyle(by: .value("Ausgewählter Monat", "Vergangen"))
+                            }
+                        }
+                        .chartForegroundStyleScale([
+                            "Vergangen": .teal, "Ausgewählt": .blue
+                        ])
+                    }.padding().frame(width: geo.size.width/2.25)
+                }
             }.padding()
         }
     }
-    func salesPerStudent(_ student: Student, selectedMonth: Binding<Int>)->Double{
+    func salesPerStudent(_ student: Student, selectedMonth: Int)->Double{
         var sales = 0.0
         let costsPerMinute = Double(student.payment)/60.0
         if !showAllSales{
-            let startOfMonth = MonthNavigator.getDateFromComponents(month: selectedMonth.wrappedValue).startOfMonth()
-            let endOfMonth = MonthNavigator.getDateFromComponents(month: selectedMonth.wrappedValue).endOfMonth()
+            let startOfMonth = MonthNavigator.getDateFromComponents(month: selectedMonth).startOfMonth()
+            let endOfMonth = MonthNavigator.getDateFromComponents(month: selectedMonth).endOfMonth()
             for lesson in student.lessons.filter("date > %@ && date < %@", startOfMonth as CVarArg, endOfMonth as CVarArg){
                 sales += costsPerMinute * Double(lesson.duration)
             }
@@ -111,10 +127,10 @@ struct SalesView: View {
         }
         return sales
     }
-    func salesTotal()->Double{
+    func salesTotal(_ selectedMonth: Int)->Double{
         var sales = 0.0
         for student in students{
-            sales += salesPerStudent(student, selectedMonth: $selectedMonth)
+            sales += salesPerStudent(student, selectedMonth: selectedMonth)
         }
         return sales
     }
